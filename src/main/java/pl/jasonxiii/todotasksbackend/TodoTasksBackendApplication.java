9befcb3de379eb5a27c63740605c95638c9d9a76
Jason <numberthirteen4026@gmail.com>
2023-02-19 13:56:44 +0100

    Initial commit

---
+++ src/main/java/pl/jasonxiii/todotasksbackend/TodoTasksBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.jasonxiii.todotasksbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoTasksBackendApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(TodoTasksBackendApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/jasonxiii/todotasksbackend/TodoTasksBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jasonxiii.todotasksbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoTasksBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
