5f737667cec27d18ae05c3f417cb0d163213a940
Jason <numberthirteen4026@gmail.com>
2023-02-19 14:35:49 +0100

    Add a WebMvcConfigurer method in the main class

--- src/main/java/pl/jasonxiii/todotasksbackend/TodoTasksBackendApplication.java
+++ src/main/java/pl/jasonxiii/todotasksbackend/TodoTasksBackendApplication.java
@@ -2,6 +2,9 @@ package pl.jasonxiii.todotasksbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TodoTasksBackendApplication
@@ -9,5 +12,18 @@ public class TodoTasksBackendApplication
 	public static void main(String[] args)
 	{
 		SpringApplication.run(TodoTasksBackendApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer configure()
+	{
+		return new WebMvcConfigurer()
+		{
+			@Override
+			public void addCorsMappings(CorsRegistry registry)
+			{
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
 	}
 }
