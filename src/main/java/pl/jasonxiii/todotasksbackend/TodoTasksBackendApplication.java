055f8530dfbc52bf04a8ad352d901a30026000a1
Jason <numberthirteen4026@gmail.com>
2023-02-19 14:43:40 +0100

    Optimise imports in the main class

--- src/main/java/pl/jasonxiii/todotasksbackend/TodoTasksBackendApplication.java
+++ src/main/java/pl/jasonxiii/todotasksbackend/TodoTasksBackendApplication.java
@@ -1,10 +1,10 @@
 package pl.jasonxiii.todotasksbackend;
 
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TodoTasksBackendApplication
